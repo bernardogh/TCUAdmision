8994dce202bc87016053ae881183c931b557f325
alelizmu <jose.lizano@urcso.info>
2020-07-13 17:16:49 -0600

    WS Users

--- Administrator/app/rest_api.py
+++ Administrator/app/rest_api.py
@@ -17,3 +17,14 @@ def question():
     for question in data:
         items.append({"id":question[0], "img": question[1], "enlace": question[2], "texto": question[3]})
     return jsonify({'questions':items})
+
+
+@app.route('/users', methods=["GET"])
+def users():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM USUARIO')
+    data = cur.fetchall()
+    items = []
+    for user in data:
+        items.append({"USU_CODIGO_USUARIO":user[0], "USU_NOMBRE": user[1], "USU_APELLIDO": user[2], "USU_CORREO": user[3], "USU_TIPO": user[5], })
+    return jsonify({'users':items})
