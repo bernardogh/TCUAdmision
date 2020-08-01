33bbaefe738280a0b300b2a2188e956a6c40ccd5
JoJoLa <jonathan.arroyo@ucrso.info>
2020-08-01 12:42:15 -0600

    Casi que todo listo, revisar detalles nada más

--- Administrator/app/admin_views.py
+++ Administrator/app/admin_views.py
@@ -37,7 +37,7 @@ def dashboard():
         else:
             if data[0][5] == 'ADMIN':
                 if not session.get("USERNAME") is None:
-                    flash("Ya tienes una sesion iniciada")
+                    flash("Ya tienes una sesión iniciada")
                     return render_template('admin/dashboardIndex.html', user=data[0][3], counter_users=counter_users[0][0], counter_questions=counter_questions[0][0], counter_exam=counter_exam[0][0], counter_int=counter_int[0][0])
                 else:
                     session["USERNAME"] = user
@@ -67,7 +67,7 @@ def dashboard():
             counter_int = cur.fetchall()
             return render_template('admin/dashboardIndex.html', user=data[0][3], counter_users=counter_users[0][0], counter_questions=counter_questions[0][0], counter_exam=counter_exam[0][0], counter_int=counter_int[0][0])
         else:
-            flash('Debes iniciar sesion...!!!', 'danger')
+            flash('Debes iniciar sesión...!!!', 'danger')
             return redirect(url_for('login'))
     return render_template('admin/dashboardIndex.html', user='aizehn')
 
@@ -82,7 +82,7 @@ def create_user():
     if not session.get("USERNAME") is None:
         return render_template('admin/create_user.html', user=session.get("USERNAME"))
     else:
-        flash('Debes iniciar sesion...!!!')
+        flash('Debes iniciar sesión...!!!')
         return redirect(url_for("login"))
 
 @app.route("/dashboard/addexam")
@@ -90,7 +90,7 @@ def create_exam():
     if not session.get("USERNAME") is None:
         return render_template('admin/addexam.html', user=session.get("USERNAME"))
     else:
-        flash('Debes iniciar sesion...!!!')
+        flash('Debes iniciar sesión...!!!')
         return redirect(url_for("login"))
 
 @app.route("/dashboard/exams/modify/<testID>")
@@ -108,7 +108,7 @@ def modifyTest(testID):
     cur = mysql.get_db().cursor()
     cur.execute('UPDATE EXAMEN SET NOMBRE=%s, ANHO=%s WHERE EXM_CODIGO = %s', (name, year, testID))
     mysql.get_db().commit()
-    flash(u'El examen se modificó existosamente', 'success')
+    flash(u'El examen se modificó exitosamente', 'success')
     return redirect(url_for('exams'))
 
 @app.route("/dashboard/create_user/save", methods=["POST"])
@@ -121,8 +121,8 @@ def saveuser():
     cur = mysql.get_db().cursor()
     cur.execute('INSERT INTO USUARIO (USU_NOMBRE, USU_APELLIDO, USU_CORREO, USU_CONTRASENHA, USU_TIPO) VALUES (%s, %s, %s, %s, %s)', (name, lastname, user, passw, admin))
     mysql.get_db().commit()
-    flash(u'El usuario se guardo existosamente', 'success')
-    return redirect(url_for("dashboard"))
+    flash(u'El usuario se registró exitosamente', 'success')
+    return redirect(url_for("modify_user"))
 
 @app.route("/dashboard/modify_user")
 def modify_user():
@@ -145,7 +145,7 @@ def examsAdd():
     cur = mysql.get_db().cursor()
     cur.execute('INSERT INTO EXAMEN (ANHO, NOMBRE) VALUES (%s,%s)', (year, name))
     mysql.get_db().commit()
-    flash(u'El examen se guardo existosamente', 'success')
+    flash(u'El examen se registró exitosamente', 'success')
     cur.execute('SELECT * FROM EXAMEN')
     data = cur.fetchall()
     return render_template('admin/exams.html', exams = data)
@@ -168,7 +168,7 @@ def muser(id):
     cur = mysql.get_db().cursor()
     cur.execute('UPDATE USUARIO SET USU_NOMBRE=%s, USU_APELLIDO=%s, USU_CORREO=%s, USU_CONTRASENHA=%s, USU_TIPO=%s WHERE USU_CODIGO_USUARIO= %s', (name, lastname, user, passw, admin, id))
     mysql.get_db().commit()
-    flash(u'El usuario se modifico existosamente', 'success')
+    flash(u'El usuario se modificó exitosamente', 'success')
     return redirect(url_for("modify_user"))
 
 @app.route('/dashboard/delete/<id>')
@@ -176,16 +176,27 @@ def delUser(id):
     cur = mysql.get_db().cursor()
     cur.execute('DELETE FROM USUARIO WHERE USU_CODIGO_USUARIO = %s', (id,))
     mysql.get_db().commit()
-    flash(u'El usuario se elimino existosamente', 'success')
+    flash(u'El usuario se eliminó exitosamente', 'success')
     return redirect(url_for("modify_user"))
 
 @app.route('/dashboard/deleteTest/<id>')
 def delTest(id):
     cur = mysql.get_db().cursor()
+    cur.execute('DELETE FROM EX_CONTIENE_PRE WHERE EXM_CODIGO = %s', (id,))
     cur.execute('DELETE FROM EXAMEN WHERE EXM_CODIGO = %s', (id,))
     mysql.get_db().commit()
-    flash(u'El examen se eliminó existosamente', 'success')
+    flash(u'El examen se eliminó exitosamente', 'success')
     return redirect(url_for("exams"))
+
+@app.route('/dashboard/deleteQuestion/<id>,<examID>')
+def delQuestion(id, examID):
+    cur = mysql.get_db().cursor()
+    cur.execute('DELETE FROM EX_CONTIENE_PRE WHERE CODIGO_PREGUNTA = %s', (id,))
+    cur.execute('DELETE FROM PRES_COMPUESTA_RESP WHERE CODIGO_PREGUNTA = %s', (id,))
+    cur.execute('DELETE FROM PREGUNTA WHERE CODIGO_PREGUNTA = %s', (id,))
+    mysql.get_db().commit()
+    flash(u'La pregunta se eliminó exitosamente', 'success')
+    return redirect(url_for("modifyquestion", testID = examID))
 
 
 def allowed_image(filename):
